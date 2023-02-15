110aafd32ca26971231ce0ff223b54b6f2c56d0a
Zachary Spalding <spalding@senylrc.org>
2023-02-15 10:21:19 -0500

    exception for state library for items to loan

--- cdlc_script/cdlc_function.php
+++ cdlc_script/cdlc_function.php
@@ -93,7 +93,7 @@ function shipmtotxt($shipmethod)
     if ($shipmethod=="crb") {
         $shiptxt='Capital Region BOCES Courier';
     }
-    if ($shipmethod=='OCLC Article Exchange'){
+    if ($shipmethod=='OCLC Article Exchange') {
         $shiptxt='OCLC Article Exchange';
     }
     if ($shipmethod=="other") {
@@ -144,15 +144,15 @@ function selected($days, $filter_value)
 function elementHunt($startdated, $hunting)
 {
     switch ($hunting) {
-        case "D":
-            $hunted = substr($startdated, 3, 2);
-            break;
-        case "M":
-            $hunted = substr($startdated, 0, 2);
-            break;
-        case "Y":
-            $hunted = substr($startdated, 6, 4);
-            break;
+    case "D":
+        $hunted = substr($startdated, 3, 2);
+        break;
+    case "M":
+        $hunted = substr($startdated, 0, 2);
+        break;
+    case "Y":
+        $hunted = substr($startdated, 6, 4);
+        break;
     }
     return $hunted;
 }
@@ -173,26 +173,26 @@ function returnLimits($Offset, $filter_numresults)
     }
     $endint = $startint + $filter_numresults;
 }
-#start of eform Functions
+// start of eform Functions
 function normalize_availability($itemavail)
 {
     $itemavail = str_replace(" ", "", $itemavail);
     $itemavail = str_replace("\n", "", $itemavail);
     switch ($itemavail) {
-        case "-":
-            return 1;
+    case "-":
+        return 1;
             break;
-        case "AVAILABLE":
-            return 1;
+    case "AVAILABLE":
+        return 1;
             break;
-        case "Available":
-            return 1;
+    case "Available":
+        return 1;
             break;
-        case "CheckedIn":
-            return 1;
+    case "CheckedIn":
+        return 1;
             break;
-        default:
-            return 0;
+    default:
+        return 0;
     }
 }
 function set_availability($itemavail)
@@ -223,84 +223,84 @@ function set_koha_availability($itemavail)
 function find_catalog($location)
 {
     switch ($location) {
-        case "CDLC Partial Union Catalog (PUC)":
-            return "Koha";
+    case "CDLC Partial Union Catalog (PUC)":
+        return "Koha";
             break;
-        case "HFM BOCES":
-            return "Alexandria";
+    case "HFM BOCES":
+        return "Alexandria";
             break;
-        case "Albany College of Pharmacy and Health Sciences":
-            return "Worldcat";
+    case "Albany College of Pharmacy and Health Sciences":
+        return "Worldcat";
             break;
-        case "New York State Department of Health":
-            return "OPALS";
+    case "New York State Department of Health":
+        return "OPALS";
             break;
-        case "Siena College":
-            return "Alma";
+    case "Siena College":
+        return "Alma";
             break;
-        case "Albany Law School":
-            return "Symphony";
+    case "Albany Law School":
+        return "Symphony";
             break;
-        case "Hudson Valley Community College":
-            return "Alma";
+    case "Hudson Valley Community College":
+        return "Alma";
             break;
-        case "University at Albany":
-            return "Alma";
+    case "University at Albany":
+        return "Alma";
             break;
 
-        case "Fulton Montgomery Community College":
-            return "Alma";
+    case "Fulton Montgomery Community College":
+        return "Alma";
             break;
-        case "Rudolf Steiner Library":
-            return "OPALS";
+    case "Rudolf Steiner Library":
+        return "OPALS";
             break;
-        case "Union College":
-            return "Alma";
+    case "Union College":
+        return "Alma";
             break;
-        case "SUNY Adirondack":
-            return "Alma";
+    case "SUNY Adirondack":
+        return "Alma";
             break;
-        case "SUNY Cobleskill":
-            return "Alma";
+    case "SUNY Cobleskill":
+        return "Alma";
             break;
-        case "Schenectady County Community College":
-            return "Alma";
+    case "Schenectady County Community College":
+        return "Alma";
             break;
-        case "Russell Sage College":
-            return "Alma";
+    case "Russell Sage College":
+        return "Alma";
             break;
-        case "Skidmore College":
-            return "Folio";
+    case "Skidmore College":
+        return "Folio";
             break;
-        case "College of Saint Rose":
-            return "Voyager";
+    case "College of Saint Rose":
+        return "Voyager";
             break;
-        case "Rensselaer Polytechnic Institute":
-            return "Worldcat";
+    case "Rensselaer Polytechnic Institute":
+        return "Worldcat";
             break;
-        case "MVLS and SALS combined catalog":
-            return "Polaris";
+    case "MVLS and SALS combined catalog":
+        return "Polaris";
             break;
-        case "Upper Hudson Library System":
-            return "Innovative";
+    case "Upper Hudson Library System":
+        return "Innovative";
             break;
-        case "WSWHE BOCES School Library System":
-            return "OPALS";
+    case "WSWHE BOCES School Library System":
+        return "OPALS";
             break;
-        case "Scotia Glenville School District":
-            return "OPALS";
+    case "Scotia Glenville School District":
+        return "OPALS";
             break;
-        case "Questar School Library System":
-            return "OPALS";
+    case "Questar School Library System":
+        return "OPALS";
             break;
-        case "Capital Region BOCES":
-            return "SirsiDynix";
+    case "Capital Region BOCES":
+        return "SirsiDynix";
             break;
-        case "Emma Willard":
-            return "SirsiDynix";
+    case "Emma Willard":
+        return "SirsiDynix";
             break;
-        case "New York State Library":
-            return "SirsiDynix";
+    case "New York State Library":
+        return "SirsiDynix";
             break;
 
     }
@@ -311,7 +311,7 @@ function find_locationinfo($locationalias, $locationname)
     $locationalias=trim($locationalias);
     $locationname=trim($locationname);
     $libparticipant='';
-    require '/var/www/cdlc_script/cdlc_db.inc';
+    include '/var/www/cdlc_script/cdlc_db.inc';
     $db = mysqli_connect($dbhost, $dbuser, $dbpass);
     mysqli_select_db($db, $dbname);
     if ($locationname == "MVLS and SALS combined catalog") {
@@ -321,10 +321,10 @@ function find_locationinfo($locationalias, $locationname)
     } else {
         $GETLISTSQL="SELECT `loc`,`participant`,`ill_email`,`suspend`,`system`,`Name`,`alias` FROM `$cdlcLIB` where alias = '$locationalias' ";
     }
-    #for test list of libraries on request page
-    #echo $GETLISTSQL."<br>";
-    #echo $locationalias."<br>";
-    #echo $locationname."<br>";
+    // for test list of libraries on request page
+    // echo $GETLISTSQL."<br>";
+    // echo $locationalias."<br>";
+    // echo $locationname."<br>";
 
     $result=mysqli_query($db, $GETLISTSQL);
     $row = mysqli_fetch_row($result);
@@ -334,48 +334,53 @@ function find_locationinfo($locationalias, $locationname)
 
 function check_itemtype($destill, $itemtype)
 {
-    require '/var/www/cdlc_script/cdlc_db.inc';
+    include '/var/www/cdlc_script/cdlc_db.inc';
     $db = mysqli_connect($dbhost, $dbuser, $dbpass);
     mysqli_select_db($db, $dbname);
-    $GETLISTSQL="SELECT book_loan,av_loan,ejournal_request,theses_loan,ebook_request FROM `$cdlcLIB` where loc = '$destill' ";
+    $GETLISTSQL="SELECT `Name`,book_loan,av_loan,ejournal_request,theses_loan,ebook_request FROM `$cdlcLIB` where loc = '$destill' ";
     $result=mysqli_query($db, $GETLISTSQL);
     while ($row = $result->fetch_assoc()) {
+        $libname=$row['Name'];
+        if (strpos($libname, 'New York State Library')!== false) {
+            // allow all items for the NY State Library at their request
+            return 1;
+        }
         if ((strcmp($itemtype, 'book') == "No") || (strcmp($itemtype, 'book (large print)') == "No")) {
-            #See if  request is for a book
+            // See if  request is for a book
             if ($row['book_loan']=="Yes") {
-                #Checking if book is allowed
+                // Checking if book is allowed
                 return 1;
             }
         }
         if ((strcmp($itemtype, 'journal') == "No") || (strcmp($itemtype, 'journal (electronic)') == "No")) {
-            #See if  request is for a journal
+            // See if  request is for a journal
             if ($row['ejournal_request']=="Yes") {
-                #Checking if journal is allowed
+                // Checking if journal is allowed
                 return 1;
             }
         }
         if ((strcmp($itemtype, 'book (electronic)') == "No") || (strcmp($itemtype, 'web') == "No")) {
-            #See if  request is for ebook
+            // See if  request is for ebook
             if ($row['ebook_request']=="Yes") {
-                #Checking if e-book is allowed
+                // Checking if e-book is allowed
                 return 1;
             }
         }
         if ((strpos($itemtype, 'recording') !== false) || (strpos($itemtype, 'video') !== false) || (strpos($itemtype, 'audio') !== false)) {
-            #See if  request is  audio video related
+            // See if  request is  audio video related
             if ($row['av_loan']=="Yes") {
-                #Checking if AV is allowed
+                // Checking if AV is allowed
                 return 1;
             }
         }
         if ((strcmp($itemtype, 'other') == 0) || (strcmp($itemtype, 'music-score') == "No") || (strcmp($itemtype, 'map') == "No") || (strcmp($itemtype, 'other (electronic)') == "No")) {
-            #See if  request is for reference
+            // See if  request is for reference
             if ($row['theses_loan']=="Yes") {
-                #Checking if reference is allowed
+                // Checking if reference is allowed
                 return 1;
             }
         }
     }
-    return 0; #Matched none of the above
-} # end check_itemtype
+    return 0; // Matched none of the above
+} // end check_itemtype
 ?>
